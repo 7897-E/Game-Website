344dd03eb9073fb7493af4cb8df9cd4dfc4b2efa
Emir <74026514+7897-E@users.noreply.github.com>
2023-11-13 15:42:42 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const gameListContainer = document.getElementById("gameList");
+    const overlay = document.querySelector(".overlay");
+
+    // Fetch game data from JSON file
+    fetch('games.json')
+        .then(response => response.json())
+        .then(games => {
+            // Loop through the games array and dynamically create HTML for each game
+            games.forEach(game => {
+                const gameElement = document.createElement("section");
+                gameElement.classList.add("game");
+
+                const gameButton = document.createElement("button");
+                gameButton.classList.add("game-button");
+                gameButton.addEventListener("click", function() {
+                    window.open(game.link, "_blank");
+                });
+
+                const tooltip = document.createElement("div");
+                tooltip.classList.add("tooltip");
+                tooltip.textContent = game.description;
+
+                gameButton.innerHTML = `
+                    <img src="${game.image}" alt="${game.title}">
+                    <h2>${game.title}</h2>
+                `;
+
+                gameButton.appendChild(tooltip);
+
+                gameElement.appendChild(gameButton);
+                gameListContainer.appendChild(gameElement);
+
+                gameButton.addEventListener("mouseenter", function() {
+                    overlay.style.display = "block";
+                    updateOverlayContent(game);
+                });
+
+                gameButton.addEventListener("mouseleave", function() {
+                    overlay.style.display = "none";
+                });
+            });
+        })
+        .catch(error => console.error('Error fetching games.json:', error));
+
+    function updateOverlayContent(game) {
+        overlay.innerHTML = `
+            <h2>${game.title}</h2>
+            <p>${game.description}</p>
+        `;
+    }
+});
