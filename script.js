7aa313ac04f6ca268831d188a8f7964dfcb7aed4
Emir <74026514+7897-E@users.noreply.github.com>
2023-11-15 06:24:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -70,12 +70,8 @@ document.addEventListener("DOMContentLoaded", function() {
 function positionOverlay(event, gameButton, overlay) {
     const rect = gameButton.getBoundingClientRect();
 
-    const overlayLeft = rect.left + window.pageXOffset + (rect.width / 2) - (overlay.offsetWidth / 2);
-    const overlayTop = rect.bottom + window.scrollY;
-
-    overlay.style.left = `${overlayLeft}px`;
-    overlay.style.top = `${overlayTop}px`;
+    overlay.style.left = `${rect.left + window.pageXOffset}px`;
+    overlay.style.top = `${rect.bottom + window.scrollY}px`;
 }
-
     
 });
